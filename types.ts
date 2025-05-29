cbcb2d0eb1457c203d30c45d791dcf0ca026b16b
mohammadreza502 <77703181+mohammadreza502@users.noreply.github.com>
2025-05-29 23:50:16 +0330

    Delete types.ts

--- types.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Post {
-  id: string;
-  title: string;
-  date: string; // ISO date string
-  imageUrl: string;
-  excerpt: string; // Short summary for the card
-  content: string; // Main content of the post (can be plain text or basic HTML)
-}
